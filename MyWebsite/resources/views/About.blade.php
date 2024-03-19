010c5f2e93ed63fe4a891c1d4b2c96db9254d1da
MrBrainowner <kenthendico011@gmail.com>
2024-03-19 13:22:15 +0800

    templates

---
+++ MyWebsite/resources/views/About.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.template')
+@section('section1')
+    <div class="content-container"></div>
+@endsection
--- MyWebsite/resources/views/index.blade.php
+++ MyWebsite/resources/views/index.blade.php
@@ -1,1 +1,10 @@
 @extends('layouts.template')
+@section('section1')
+<div class="hero-container">
+    <div class="hero-box col-1"></div>
+    <div class="hero-box col-2">
+        <img class="hero-img" src="{{'images/111.jpg'}}" alt="Leaves Image">
+    </div>
+</div>
+<div class="content-container"></div>
+@endsection
